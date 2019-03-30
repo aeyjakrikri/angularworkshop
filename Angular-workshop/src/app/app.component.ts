cdb47699036d5cbbb40b9cc65319d163a191f842
Saranyu <saranyu.sst@gmail.com>
2019-03-30 16:31:25 +0700

    update

--- Angular-workshop/src/app/app.component.ts
+++ Angular-workshop/src/app/app.component.ts
@@ -6,56 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-workshop';
-  score: number = 49;
-  name: string = "นาย ก นามสกุล ข";
-
-  student:any = {
-    studentId: "5721602287",
-    name: "Peter",
-    weight: 57,
-    height: 175
-  }
-
-  studentList = [{
-    studentId: "5721602287",
-    name: "Peter",
-    weight: 57,
-    height: 175
-  }, {
-    studentId: "5721601111",
-    name: "Marry",
-    weight: 45,
-    height: 175
-  }, {
-    studentId: "5721602222",
-    name: "Jany",
-    weight: 55,
-    height: 165
-  }, {
-    studentId: "5721602299",
-    name: "Jam",
-    weight: 55,
-    height: 165
+  productList = [{
+    pId : "0001",
+    pName :"แปลงสีฟัน",
+    pCost:200
+  },{
+    pId : "0002",
+    pName :"แก้วน้ำ",
+    pCost:10
+  },{
+    pId : "0003",
+    pName :"ยาสีฟัน",
+    pCost:20
   }];
 
+  selectedProduct :any;
+
+  selectProduct(p){
+  this.selectedProduct = p; 
+  }
   constructor() {
-    this.student.bmi = (this.student.weight/
-      ((this.student.height/100)*(this.student.height/100))).toFixed(2);
-
-
-      
-      this.studentList.map((object,index)=>{
-        let obj:any = object;
-        obj.bmi = (object.weight/((object.height/100)*(object.height/100))).toFixed(2);
-        return obj;
-      })
-    // this.studentList.map((object,index)=>{
-    //   let obj:any = object;
-    //   obj.bmi = (object.weight/((object.height/100)*(object.height/100))).toFixed(2);
-    //     return obj;
-    // });
-    console.log(this.studentList);
     
   }
 
