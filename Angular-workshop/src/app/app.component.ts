9e6098b1ef07f81e08a86e10e8c3f9364f188307
Saranyu <saranyu.sst@gmail.com>
2019-03-31 10:38:02 +0700

    update

--- Angular-workshop/src/app/app.component.ts
+++ Angular-workshop/src/app/app.component.ts
@@ -9,15 +9,18 @@ export class AppComponent {
   productList = [{
     pId : "0001",
     pName :"แปลงสีฟัน",
-    pCost:200
+    pCost:30,
+    img : "https://www.ningnongmistine.com/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/3/8/38008.jpg"
   },{
     pId : "0002",
     pName :"แก้วน้ำ",
-    pCost:10
+    pCost:10,
+    img : "../assets/img/img1.jpg"
   },{
     pId : "0003",
     pName :"ยาสีฟัน",
-    pCost:20
+    pCost:20,
+    img : "../assets/img/img2.jpg"
   }];
 
   selectedProduct :any;
