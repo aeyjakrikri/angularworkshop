caccaf228c19712e0788ededb9bd285ce77d5723
Saranyu <saranyu.sst@gmail.com>
2019-03-31 09:08:36 +0700

    update

--- Angular-workshop/src/app/app.component.ts
+++ Angular-workshop/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonServiceService } from './common-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,57 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-workshop';
-  score: number = 49;
-  name: string = "นาย ก นามสกุล ข";
-
-  student:any = {
-    studentId: "5721602287",
-    name: "Peter",
-    weight: 57,
-    height: 175
+  request = {
+    key1:0,
+    key2:0
   }
-
-  studentList = [{
-    studentId: "5721602287",
-    name: "Peter",
-    weight: 57,
-    height: 175
-  }, {
-    studentId: "5721601111",
-    name: "Marry",
-    weight: 45,
-    height: 175
-  }, {
-    studentId: "5721602222",
-    name: "Jany",
-    weight: 55,
-    height: 165
-  }, {
-    studentId: "5721602299",
-    name: "Jam",
-    weight: 55,
-    height: 165
-  }];
-
-  constructor() {
-    this.student.bmi = (this.student.weight/
-      ((this.student.height/100)*(this.student.height/100))).toFixed(2);
-
-
-      
-      this.studentList.map((object,index)=>{
-        let obj:any = object;
-        obj.bmi = (object.weight/((object.height/100)*(object.height/100))).toFixed(2);
-        return obj;
-      })
-    // this.studentList.map((object,index)=>{
-    //   let obj:any = object;
-    //   obj.bmi = (object.weight/((object.height/100)*(object.height/100))).toFixed(2);
-    //     return obj;
-    // });
-    console.log(this.studentList);
-    
+  result:number=null;
+  constructor(private service: CommonServiceService) {
+ 
+  }
+  getData() {
+    this.service.getData(this.request).subscribe((response:any) => {
+      this.result = response.result;
+    });
   }
 
 }
--- Angular-workshop/src/app/app.module.ts
+++ Angular-workshop/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule,/*เพิ่มใหม่*/TransferState } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+// เพิ่มใหม่
+import { HttpClientModule } from "@angular/common/http";
+import { TransferHttpService } from "@gorniv/ngx-transfer-http";
+import { CommonServiceService } from './common-service.service';
+// เพิ่มใหม่
 @NgModule({
   declarations: [
     AppComponent
@@ -14,9 +17,10 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
+    HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [TransferHttpService, TransferState, CommonServiceService], //เพิ่มใหม่
   bootstrap: [AppComponent]
 })
 export class AppModule { }
